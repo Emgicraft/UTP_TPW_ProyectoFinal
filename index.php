73130dcc85db08b0feb013486d93ca6e6cc0e002
ThalyCaysa <167721227+ThalyCaysa@users.noreply.github.com>
2024-04-21 18:34:14 -0500

    Añadi lorem
    Añadi la etiqueta <p>
    baje la etiqueta <hr>

--- index.php
+++ index.php
@@ -6,10 +6,21 @@
 	<title>Sistema de Ventas</title>
 </head>
 <body>
-	<h1>MENÚ PRINCIPAL</h1><hr>
+	<h1>MENÚ PRINCIPAL</h1>
+	<hr>
 	
 	<a href="cliente">Cliente</a>
 	<a href="producto">Producto</a>
 	<a href="ventas">Ventas</a>
+
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+	</p>
 </body>
 </html>
