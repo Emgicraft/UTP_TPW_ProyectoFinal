2b56d74b2855df6fe2b941ec36167495bb01be57
Magh <63615725+Emgicraft@users.noreply.github.com>
2024-04-26 17:37:57 -0500

    Esilos agregados.

--- index.php
+++ index.php
@@ -4,27 +4,51 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Sistema de Ventas</title>
+	<link rel="stylesheet" type="text/css" href="estilos/header_footer.css">
+	<style>
+        body {
+            background: radial-gradient(circle, #004D77, #75CEFF);
+        }
+
+        main {
+            background-color: #1F1F1F;
+            text-decoration: none;
+	        color: aquamarine;
+            max-width: 1200px;
+            margin: 0 auto;
+            border-style: solid;
+            border-right-width: 10px;
+            border-left-width: 10px;
+            border-color: #0A152F;
+            padding: 10px 10px 10px 10px;
+        }
+    </style>
 </head>
 <body>
-	<h1>MENÚ PRINCIPAL</h1>
+<header class=cabezaWeb>
+	<div>
+		<img src="media/Logo_MaghTech.png" alt="Logo_MaghTech" width="100px">
+	</div>
+	<nav class="barraDeNavegacion">
+		<a href="cliente">Cliente</a>
+		<a href="producto">Producto</a>
+		<a href="ventas">Ventas</a>
+	</nav>
+</header>
+<main>
+	<h1>Título</h1>
 	<hr>
-	
-	<a href="cliente">Cliente</a>
-	<a href="producto">Producto</a>
-	<a href="ventas">Ventas</a>
-
-	<p>
-		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-
-	</p>
-
-	<h2>Titulo de prueba</h2>
-
-	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat nostrum at omnis corporis doloribus reprehenderit, repellat, tempore quidem laudantium delectus, quos odio? Tempore, animi? Dolore veritatis corrupti totam eius unde.</p>
+</main>
+<footer>
+	<div>
+        <h4>Contenido</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis possimus odio repudiandae voluptatibus autem libero consequatur veniam aliquid. Illum in repellat illo enim excepturi unde ad dicta iusto odit est?</p>
+    </div>
+    <hr>
+    <div class="derechoCopy">
+        <img src="media/Logo_MaghTech.png" alt="Logo_MaghTech" width="100px">
+        <p>Copyright© 2023-2024 MaghTech. Todos los derechos reservados.</p>
+    </div>
+</footer>
 </body>
 </html>
