b8dccf2301eef490bcf31127962233c506c1f125
NeryDragon <123653161+NeryDragon@users.noreply.github.com>
2024-05-10 23:13:46 -0500

    update interfaz home y css

--- index.php
+++ index.php
@@ -1,43 +1,75 @@
 <!DOCTYPE html>
 <html lang="es-PE">
+
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>MENÚ PRINCIPAL</title>
-</head>
-<body>
-	<h1>MENÚ PRINCIPAL</h1><hr>
-	
-	<a href="cliente">Cliente</a>
-	<a href="producto">Producto</a>
-	<a href="ventas">Ventas</a>
+	<link rel="stylesheet" href="reset.css">
+	<link rel="stylesheet" href="style.css">
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
 
+</head>
+
+<body>
+	<h1>MENÚ PRINCIPAL</h1>
+	<hr>
+
+	<header>
+
+		<div class="caja">
+			<h1><img src="imagenes/logo.png"></h1>
+
+			<nav>
+				<ul>
+					<li><a href="cliente">Cliente</a></li>
+					<li><a href="producto">Producto</a></li>
+					<li><a href="ventas">Ventas</a></li>
+				</ul>
+			</nav>
+		</div>
+
+	</header>
 
 	<img width="100%" src="banner/banner2.jpg">
 
+	<main>
 
+		<section class="principal">
 
-	<section align="center">
-	<h3>Nuestra Ubicación</h3>
-	<p>Nuestro establecimiento esta ubicado en el corazón de la ciudad</p>
-	<div>
-	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3901.6594109728962!2d-77.04738099999994!3d-12.066938899999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c8e7a8555555%3A0x9c20cb5457697ebb!2sLa%20Rambla%20-%20Brasil!5e0!3m2!1ses-419!2spe!4v1713541256053!5m2!1ses-419!2spe" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-	</div>
+			<h2 class="titulo-principal">Sobre Sabor&Frescura</h2>
+
+			<img class="utensilios" src="imagenes/Embutidos.jpg" alt="Utensilios de un barbero.">
+
+			<p>Ubicada en el corazón de la ciudad, minimarkt <strong>Sabor&Frescura</strong> trae para el mercado lo mejor en embutidos y abarrotes. Fundada en 2020, la Carnicería Sabor&Frescura ya es un referente en la ciudad y atrae nuevos clientes diariamente.</p>
+
+			<p id="mision"><em>Nuestra misión es: <strong>"Brindar productos frescos y de calidad para satisfacer las necesidades de nuestros clientes".</strong></em></p>
+
+			<p>Ofrecemos una amplia variedad de embutidos y productos de abarrotes, seleccionados con cuidado para garantizar la frescura y calidad que nuestros clientes merecen. Nuestro equipo está compuesto por profesionales experimentados que están siempre atentos a las últimas tendencias en productos y preferencias del cliente. Nuestro servicio se caracteriza por su excelencia y eficiencia, asegurando la satisfacción de nuestros clientes en cada visita.</p>
+
+	</main>
+
+	<section class="mapa" align="center">
+		<h3>Nuestra Ubicación</h3>
+		<p>Nuestro establecimiento esta ubicado en el corazón de la ciudad</p>
+		<div class="mapa-contenido">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3901.6594109728962!2d-77.04738099999994!3d-12.066938899999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c8e7a8555555%3A0x9c20cb5457697ebb!2sLa%20Rambla%20-%20Brasil!5e0!3m2!1ses-419!2spe!4v1713541256053!5m2!1ses-419!2spe" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+		</div>
 	</section>
 
-	<section>
-	<div align="center">
-	<iframe width="560" height="315" src="https://www.youtube.com/embed/ARHXAj-n18Q?si=wDvGsMQWY47u4p8k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-	</div>
+	<section class="diferenciales">
+		<div class="video" align="center">
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/ARHXAj-n18Q?si=wDvGsMQWY47u4p8k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+		</div>
 	</section>
 
 
 	<!-- Pie de pagina -->
-
 	<footer align="center">
-	<img src="imagenes/PiePAgina.png">
-	<p>&copy Copyright CATmigos - 2024</p>
+		<img src="imagenes/PiePAgina.png">
+		<p class="copyrigth">&copy Copyright Sabor&Frescura - 2024</p>
 	</footer>
 
 </body>
+
 </html>
