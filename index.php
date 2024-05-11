e01a0bff5b0963f54b0833fbd780e78f13427640
NeryDragon <123653161+NeryDragon@users.noreply.github.com>
2024-05-10 20:40:03 -0500

    update banner, mapa, video , pie de pagina

--- index.php
+++ index.php
@@ -13,13 +13,29 @@
 	<a href="ventas">Ventas</a>
 
 
-	<img width="100%" src="banner/banner.jpg">
+	<img width="100%" src="banner/banner2.jpg">
+
+
+
+	<section align="center">
+	<h3>Nuestra Ubicación</h3>
+	<p>Nuestro establecimiento esta ubicado en el corazón de la ciudad</p>
+	<div>
+	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3901.6594109728962!2d-77.04738099999994!3d-12.066938899999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c8e7a8555555%3A0x9c20cb5457697ebb!2sLa%20Rambla%20-%20Brasil!5e0!3m2!1ses-419!2spe!4v1713541256053!5m2!1ses-419!2spe" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+	</div>
+	</section>
+
+	<section>
+	<div align="center">
+	<iframe width="560" height="315" src="https://www.youtube.com/embed/ARHXAj-n18Q?si=wDvGsMQWY47u4p8k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+	</div>
+	</section>
 
 
 	<!-- Pie de pagina -->
 
 	<footer align="center">
-	<img src="imagenes/PiePAgina.jpg">
+	<img src="imagenes/PiePAgina.png">
 	<p>&copy Copyright CATmigos - 2024</p>
 	</footer>
 
