85740bfda5a339351c1c5bba6bd5f1397f8e2268
NeryDragon <123653161+NeryDragon@users.noreply.github.com>
2024-05-17 20:46:31 -0500

    Revisando cambios
    Se agrego variacion en pie de pagina, en body, header.

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="es-PE">
+
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Sistema de Ventas</title>
-	<link rel="stylesheet" type="text/css" href="estilos/header_footer.css">
-	<style>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sistema de Ventas</title>
+    <link rel="stylesheet" type="text/css" href="estilos/header_footer.css">
+    <style>
         body {
             background: radial-gradient(circle, #004D77, #75CEFF);
         }
@@ -13,7 +14,7 @@
         main {
             background-color: #1F1F1F;
             text-decoration: none;
-	        color: aquamarine;
+            color: aquamarine;
             max-width: 1200px;
             margin: 0 auto;
             border-style: solid;
@@ -23,32 +24,105 @@
             padding: 10px 10px 10px 10px;
         }
     </style>
+
+
+
+    <!--Por validar si queda -->
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
+    <!--Por validar si queda -->
+
+
+
 </head>
+
 <body>
-<header class=cabezaWeb>
-	<div>
-		<img src="media/Logo_MaghTech.png" alt="Logo_MaghTech" width="100px">
-	</div>
-	<nav class="barraDeNavegacion">
-		<a href="cliente">Cliente</a>
-		<a href="producto">Producto</a>
-		<a href="ventas">Ventas</a>
-	</nav>
-</header>
-<main>
-	<h1>Título</h1>
-	<hr>
-</main>
-<footer>
-	<div>
+    <header class=cabezaWeb>
+        <div>
+            <img src="media/Logo_MaghTech.png" alt="Logo_MaghTech" width="100px">
+        </div>
+        <nav class="barraDeNavegacion">
+            <a href="cliente">Cliente</a>
+            <a href="producto">Producto</a>
+            <a href="ventas">Ventas</a>
+        </nav>
+    </header>
+
+
+    <main>
+        <h1>Título</h1>
+        <hr>
+    </main>
+
+
+
+    <!--Por validar si queda -->
+    <header>
+        <div class="caja">
+            <h1><img src="imagenes/logo.png"></h1>
+            <nav>
+                <ul>
+                    <li><a href="cliente">Cliente</a></li>
+                    <li><a href="producto">Producto</a></li>
+                    <li><a href="ventas">Ventas</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <img width="100%" src="banner/banner2.jpg">
+
+    <main>
+        <section class="principal">
+            <h2 class="titulo-principal">Sobre Sabor&Frescura</h2>
+            <img class="utensilios" src="imagenes/Embutidos.jpg" alt="Utensilios de un barbero.">
+            <p>Ubicada en el corazón de la ciudad, minimarkt <strong>Sabor&Frescura</strong> trae para el mercado lo mejor en embutidos y abarrotes. Fundada en 2020, la Carnicería Sabor&Frescura ya es un referente en la ciudad y atrae nuevos clientes diariamente.</p>
+            <p id="mision"><em>Nuestra misión es: <strong>"Brindar productos frescos y de calidad para satisfacer las necesidades de nuestros clientes".</strong></em></p>
+            <p>Ofrecemos una amplia variedad de embutidos y productos de abarrotes, seleccionados con cuidado para garantizar la frescura y calidad que nuestros clientes merecen. Nuestro equipo está compuesto por profesionales experimentados que están siempre atentos a las últimas tendencias en productos y preferencias del cliente. Nuestro servicio se caracteriza por su excelencia y eficiencia, asegurando la satisfacción de nuestros clientes en cada visita.</p>
+    </main>
+    <!--Por validar si queda -->
+
+
+
+    <div>
         <h4>Contenido</h4>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis possimus odio repudiandae voluptatibus autem libero consequatur veniam aliquid. Illum in repellat illo enim excepturi unde ad dicta iusto odit est?</p>
     </div>
     <hr>
-    <div class="derechoCopy">
-        <img src="media/Logo_MaghTech.png" alt="Logo_MaghTech" width="100px">
-        <p>Copyright© 2023-2024 MaghTech. Todos los derechos reservados.</p>
-    </div>
-</footer>
+
+
+
+    <!--Por validar si queda -->
+    <section class="mapa" align="center">
+        <h3>Nuestra Ubicación</h3>
+        <p>Nuestro establecimiento esta ubicado en el corazón de la ciudad</p>
+        <div class="mapa-contenido">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3901.6594109728962!2d-77.04738099999994!3d-12.066938899999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c8e7a8555555%3A0x9c20cb5457697ebb!2sLa%20Rambla%20-%20Brasil!5e0!3m2!1ses-419!2spe!4v1713541256053!5m2!1ses-419!2spe" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+    </section>
+
+    <section class="diferenciales">
+        <div class="video" align="center">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/ARHXAj-n18Q?si=wDvGsMQWY47u4p8k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </div>
+    </section>
+    <!--Por validar si queda -->
+
+
+
+
+
+    <!-- Pie de pagina -->
+    <footer align="center">
+
+        <div class="derechoCopy">
+            <img src="imagenes/PiePAgina.png">
+            <p class="derechoCopy">&copy Copyright Sabor&Frescura - 2024</p>
+        </div>
+    </footer>
+
+
 </body>
+
 </html>
