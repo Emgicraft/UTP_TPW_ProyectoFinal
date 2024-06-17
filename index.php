4d27a286f8994be87150d35a48a2b97db5a0c6fe
Magh <63615725+Emgicraft@users.noreply.github.com>
2024-06-17 18:56:08 -0500

    Estilos actualizados

--- index.php
+++ index.php
@@ -6,123 +6,70 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Sistema de Ventas</title>
     <link rel="stylesheet" type="text/css" href="estilos/header_footer.css">
-    <style>
-        body {
-            background: radial-gradient(circle, #004D77, #75CEFF);
-        }
-
-        main {
-            background-color: #1F1F1F;
-            text-decoration: none;
-            color: aquamarine;
-            max-width: 1200px;
-            margin: 0 auto;
-            border-style: solid;
-            border-right-width: 10px;
-            border-left-width: 10px;
-            border-color: #0A152F;
-            padding: 10px 10px 10px 10px;
-        }
-    </style>
-
-
-
-    <!--Por validar si queda -->
-    <link rel="stylesheet" href="reset.css">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="estilos/main.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
-    <!--Por validar si queda -->
-
-
-
 </head>
 
 <body>
-    <header class=cabezaWeb>
-        <div>
-            <img src="media/Logo_MaghTech.png" alt="Logo_MaghTech" width="100px">
-        </div>
-        <nav class="barraDeNavegacion">
-            <a href="cliente">Cliente</a>
-            <a href="producto">Producto</a>
-            <a href="ventas">Ventas</a>
-        </nav>
-    </header>
-
-
-    <main>
-        <h1>Título</h1>
-        <hr>
-    </main>
-
-
-
-    <!--Por validar si queda -->
-    <header>
-        <div class="caja">
-            <h1><img src="imagenes/logo.png"></h1>
-            <nav>
-                <ul>
-                    <li><a href="cliente">Cliente</a></li>
-                    <li><a href="producto">Producto</a></li>
-                    <li><a href="ventas">Ventas</a></li>
-                </ul>
-            </nav>
-        </div>
-    </header>
-
-    <img width="100%" src="banner/banner2.jpg">
-
-    <main>
-        <section class="principal">
-            <h2 class="titulo-principal">Sobre Sabor&Frescura</h2>
-            <img class="utensilios" src="imagenes/Embutidos.jpg" alt="Utensilios de un barbero.">
-            <p>Ubicada en el corazón de la ciudad, minimarkt <strong>Sabor&Frescura</strong> trae para el mercado lo mejor en embutidos y abarrotes. Fundada en 2020, la Carnicería Sabor&Frescura ya es un referente en la ciudad y atrae nuevos clientes diariamente.</p>
-            <p id="mision"><em>Nuestra misión es: <strong>"Brindar productos frescos y de calidad para satisfacer las necesidades de nuestros clientes".</strong></em></p>
-            <p>Ofrecemos una amplia variedad de embutidos y productos de abarrotes, seleccionados con cuidado para garantizar la frescura y calidad que nuestros clientes merecen. Nuestro equipo está compuesto por profesionales experimentados que están siempre atentos a las últimas tendencias en productos y preferencias del cliente. Nuestro servicio se caracteriza por su excelencia y eficiencia, asegurando la satisfacción de nuestros clientes en cada visita.</p>
-    </main>
-    <!--Por validar si queda -->
-
-
-
+<header class=cabezaWeb>
     <div>
+        <!-- <img src="media/Logo_MaghTech.png" alt="Logo_MaghTech" width="100px"> -->
+        <img src="media/https://i.imgur.com/d3V0t5C.png" alt="Logo_MaghTech" width="100px">
+    </div>
+    <nav class="barraDeNavegacion">
+        <a href="cliente">Clientes</a>
+        <a href="producto">Productos</a>
+        <a href="ventas">Ventas</a>
+    </nav>
+</header>
+
+<main>
+    <!-- <img id="imgBanner" width="100%" src="media/embutidos_banner.jpg" alt="Todo tipo de embutidos."> -->
+    <img id="imgBanner" width="100%" src="https://i.imgur.com/z7Kbu4I.jpg" alt="Todo tipo de embutidos.">
+
+    <div id="indice_contenido">
+    <div class="indice" style="margin-right: 20px;">
         <h4>Contenido</h4>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis possimus odio repudiandae voluptatibus autem libero consequatur veniam aliquid. Illum in repellat illo enim excepturi unde ad dicta iusto odit est?</p>
+        <ol>
+            <li>Clientes</li>
+            <li>Productos</li>
+            <li>Ventas</li>
+            <li>Reportar</li>
+        </ol>
+    </div>
+    <div>
+    <article class="principal">
+        <h1>Sabor&Frescura</h1>
+        <!-- <img id="carne_cocinada" width="35%" src="media/carne_cocinada_banner.jpg" alt="Corte de carne cocinado."> -->
+        <img id="carne_cocinada" width="35%" src="https://i.imgur.com/OADKXGi.jpg" alt="Corte de carne cocinado.">
+        <p>Ubicada en el corazón de la ciudad, minimarkt <strong>Sabor&Frescura</strong> trae para el mercado lo mejor en embutidos y abarrotes. Fundada en 2020, la Carnicería Sabor&Frescura ya es un referente en la ciudad y atrae nuevos clientes diariamente.</p>
+        <p id="mision"><em>Nuestra misión es: <strong>"Brindar productos frescos y de calidad para satisfacer las necesidades de nuestros clientes".</strong></em></p>
+        <p>Ofrecemos una amplia variedad de embutidos y productos de abarrotes, seleccionados con cuidado para garantizar la frescura y calidad que nuestros clientes merecen. Nuestro equipo está compuesto por profesionales experimentados que están siempre atentos a las últimas tendencias en productos y preferencias del cliente. Nuestro servicio se caracteriza por su excelencia y eficiencia, asegurando la satisfacción de nuestros clientes en cada visita.</p>
+    </article>
+    </div>
     </div>
     <hr>
-
-
-
-    <!--Por validar si queda -->
-    <section class="mapa" align="center">
+    <section id="mapa">
         <h3>Nuestra Ubicación</h3>
-        <p>Nuestro establecimiento esta ubicado en el corazón de la ciudad</p>
-        <div class="mapa-contenido">
+        <p>Nuestro establecimiento esta ubicado en el corazón de la ciudad.</p>
+        <div id="mapa-contenido">
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3901.6594109728962!2d-77.04738099999994!3d-12.066938899999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c8e7a8555555%3A0x9c20cb5457697ebb!2sLa%20Rambla%20-%20Brasil!5e0!3m2!1ses-419!2spe!4v1713541256053!5m2!1ses-419!2spe" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
     </section>
 
-    <section class="diferenciales">
-        <div class="video" align="center">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/ARHXAj-n18Q?si=wDvGsMQWY47u4p8k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    <section id="diferenciales">
+        <div id="difVideoYT">
+            <iframe width="720px" height="480px" src="https://www.youtube.com/embed/ARHXAj-n18Q?si=wDvGsMQWY47u4p8k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
     </section>
-    <!--Por validar si queda -->
+</main>
 
-
-
-
-
-    <!-- Pie de pagina -->
-    <footer align="center">
-
-        <div class="derechoCopy">
-            <img src="imagenes/PiePAgina.png">
-            <p class="derechoCopy">&copy Copyright Sabor&Frescura - 2024</p>
-        </div>
-    </footer>
-
-
+<!-- Pie de pagina -->
+<footer>
+    <div class="derechoCopy">
+        <img src="media/logo.png" width="7%">
+        <p class="derechoCopy">&copy Copyright Sabor&Frescura - 2024</p>
+    </div>
+</footer>
 </body>
-
 </html>
