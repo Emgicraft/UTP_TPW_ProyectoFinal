7b778c401eac27f58ec1917eafd57d3497805434
Magh <63615725+Emgicraft@users.noreply.github.com>
2024-06-21 14:05:43 -0500

    Conexion corregida

--- config/conexion.php
+++ config/conexion.php
@@ -1,7 +1,7 @@
 <?php
 	// Establecer conexión con la BD:
 	// Haciendo uso de la Tecnología PDO (PHP Data Object)
-	$cdn = "mysql:host=127.0.0.1;dbname=bdmarket";
+	$cdn = "mysql:host=127.0.0.1;dbname=BD_Market";
 	$usr = "root";
 	$clv = "";
 
