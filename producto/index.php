3066b9eada01c1a7dd999e39355368dff7f981df
Magh <63615725+Emgicraft@users.noreply.github.com>
2024-06-21 22:25:20 -0500

    Index producto actualizado

--- producto/index.php
+++ producto/index.php
@@ -4,6 +4,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Producto</title>
+	<link rel="stylesheet" href="../estilos/cabecera.css">
 	<script type="text/javascript">
 		function confirmar(ruta) {
 			var r = confirm("¿Está seguro que desea continuar?");
@@ -12,16 +13,12 @@
 	</script>
 </head>
 <body>
-	<h1>Producto</h1><br>
-
-	<table>
-		<tr><td><a href="..\">Retornar</a></td></tr>
-		<tr><td><a href="agregar_producto.php">Agregar un nuevo producto</a></td></tr>
-	</table>
-	
-	<hr>
-
-	<!-- CODIGO PHP -->
+<header>
+	<h1>Producto</h1>
+	<a href="agregar_producto.php">Agregar un nuevo producto</a>
+	<a href="..">Retornar</a>
+</header>
+<main>
 	<?php
 		// Inicializando otras variables:
 		$tabla = array();
@@ -49,44 +46,48 @@
 				// Recojemos las filas generadas en una matriz bidimensional:
 				$tabla = $sentencia->fetchAll();
 			}
-		} else {
+		}
+		else {
 			// Sino, este será el valor predeterminado:
 			$valor = "";
 		}
 	?>
 
 	<form method="get">
-		<label>Valor a buscar:</label>
-		<input type="text" name="txtValor" value="<?php echo $valor ?>">
+		<label for="descripcion">Buscar por descripción:</label>
+		<input type="text" name="txtValor" value="<?php echo $valor ?>" id="descripcion">
 		<input type="submit" value="Buscar">
 	</form>
 	<br>
-
-	<table border=1>
+	<table>
 		<tr>
-			<th>Código</th>
+			<th>ID</th>
+			<th>Código de inventario</th>
 			<th>Descripción</th>
 			<th>Categoría</th>
 			<th>Precio S/.</th>
+			<th>Stock</th>
 		</tr>
-
 	<?php
 		foreach ($tabla as $registro) {
 	?>
-
 		<tr>
-			<td><?php echo $registro["id"]; ?></td>
-			<td><?php echo $registro["descripcion"]; ?></td>
-			<td><?php echo $registro["categoria"]; ?></td>
-			<td><?php echo $registro["precio"]; ?></td>
-			<td><a href="<?php echo 'editar_producto.php?codProd='.$registro['id']; ?>">Editar</a></td>
-			<td><a href="javascript:confirmar('eliminar_producto.php?codProd=<?php echo $registro["id"]; ?>')">Eliminar</a></td>
+			<td><?php echo $registro["ID_Producto"]; ?></td>
+			<td><?php echo $registro["Codigo_inventario"]; ?></td>
+			<td><?php echo $registro["Descripcion"]; ?></td>
+			<td><?php echo $registro["Categoria_ID"]; ?></td>
+			<td><?php echo $registro["Precio"]; ?></td>
+			<td><?php echo $registro["Stock"]; ?></td>
+			<td><a href="<?php echo 'editar_producto.php?codProd=' . $registro['ID_Producto']; ?>">Editar</a></td>
+			<td><a href="javascript:confirmar('eliminar_producto.php?codProd=<?php echo $registro["ID_Producto"]; ?>')">Eliminar</a></td>
 		</tr>
-
 	<?php
 		}
 	?>
-
 	</table>
+</main>
+<footer>
+
+</footer>
 </body>
 </html>
